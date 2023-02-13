5fbe308c9a4544155e1c195a8b2aaf653584bab6
Ghoulam-Ali <rakcodeer@gmail.com>
2023-02-13 22:34:32 +0100

    gslider is done

---
+++ main.js
@@ -0,0 +1,9 @@
+new Glider(document.querySelector(".testimonials"), {
+  slidesToShow : 1,
+  dots:".dots",
+  draggable: true,
+  arrows: {
+    prev: ".slider-prev",
+    next: ".slider-next",
+  },
+});
